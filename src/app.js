9494c3c97729cbcaee69a2146eaeeccdca8c4346
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2023-05-07 20:39:20 +0530

    Update https settings

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import cors from "@fastify/cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cron from "node-cron";
-import https from "https";
 import fs from "fs";
 
 dotenv.config();
@@ -11,7 +10,24 @@ dotenv.config();
 import trackingRoutes from "./routes/tracking.routes.js";
 import { downloadMaxmindDB } from "./utils/maxmind.js";
 
-const fastify = fastifyServer();
+let fastifyOptions = {};
+
+if (process.env.SSL_KEY && process.env.SSL_CERT) {
+  const sslKey = fs.readFileSync(process.env.SSL_KEY, "utf8");
+  const sslCert = fs.readFileSync(process.env.SSL_CERT, "utf8");
+  fastifyOptions = {
+    https: {
+      key: sslKey,
+      cert: sslCert,
+    },
+  };
+}
+
+if (process.env.NODE_ENV === "production") {
+  fastifyOptions.logger = { level: "info" };
+}
+
+const fastify = fastifyServer(fastifyOptions);
 fastify.register(cors, () => {
   return (req, callback) => {
     const corsOptions = {
@@ -26,23 +42,10 @@ fastify.register(trackingRoutes, { prefix: "/v1/:analyticsId" });
 
 const start = async () => {
   try {
-    let server;
-    if (process.env.SSL_KEY && process.env.SSL_CERT) {
-      const sslKey = fs.readFileSync(process.env.SSL_KEY, "utf8");
-      const sslCert = fs.readFileSync(process.env.SSL_CERT, "utf8");
-      const sslOptions = {
-        key: sslKey,
-        cert: sslCert,
-      };
-      server = https
-        .createServer(sslOptions, fastify)
-        .listen(process.env.PORT || 3000, process.env.HOST || "127.0.0.1");
-    } else {
-      server = await fastify.listen({
-        port: process.env.PORT || 3000,
-        host: process.env.HOST || "127.0.0.1",
-      });
-    }
+    let server = await fastify.listen({
+      port: process.env.PORT || 3000,
+      host: process.env.HOST || "127.0.0.1",
+    });
     await mongoose.connect(process.env.MONGO_URL);
     downloadMaxmindDB(); // Download DB on server start
     cron.schedule("12 0 * * 3,6", downloadMaxmindDB); // Download new DB every Wednesday and Saturday at 12:00 PM
