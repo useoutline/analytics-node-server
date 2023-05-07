a876ccfeab84190490b1b273c67ba82c58a46831
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2023-05-07 20:21:03 +0530

    Add ssl certs config

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from "@fastify/cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cron from "node-cron";
+import https from "https";
+import fs from "fs";
 
 dotenv.config();
 
@@ -24,10 +26,23 @@ fastify.register(trackingRoutes, { prefix: "/v1/:analyticsId" });
 
 const start = async () => {
   try {
-    const server = await fastify.listen({
-      port: process.env.PORT || 3000,
-      host: process.env.HOST || "127.0.0.1",
-    });
+    let server;
+    if (process.env.SSL_KEY && process.env.SSL_CERT) {
+      const sslKey = fs.readFileSync(process.env.SSL_KEY, "utf8");
+      const sslCert = fs.readFileSync(process.env.SSL_CERT, "utf8");
+      const sslOptions = {
+        key: sslKey,
+        cert: sslCert,
+      };
+      server = https
+        .createServer(sslOptions, fastify)
+        .listen(process.env.PORT || 3000, process.env.HOST || "127.0.0.1");
+    } else {
+      server = await fastify.listen({
+        port: process.env.PORT || 3000,
+        host: process.env.HOST || "127.0.0.1",
+      });
+    }
     await mongoose.connect(process.env.MONGO_URL);
     downloadMaxmindDB(); // Download DB on server start
     cron.schedule("12 0 * * 3,6", downloadMaxmindDB); // Download new DB every Wednesday and Saturday at 12:00 PM
